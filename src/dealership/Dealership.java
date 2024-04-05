8b1c4a79686ae6a8b9524f00e95f294b785f54ce
Steven Mancine <steven.mancine@charter.com>
2024-04-05 15:03:04 -0600

    Add dealership and print to file function

---
+++ src/dealership/Dealership.java
@@ -0,0 +1,69 @@
+package dealership;
+import java.util.ArrayList;
+import validator.Validator;
+import java.util.Scanner;
+
+import automobile.Automobile;
+public class Dealership {
+  private String name;
+  private ArrayList<Automobile> inventory;
+  
+  public Dealership(String name) {
+	  this.name = name;
+	  this.inventory = new ArrayList<>();
+  }
+  
+  // Getters
+  public String getName() {
+	  return this.name;
+  }
+  
+  public ArrayList<Automobile> getInventory() {
+	  return this.inventory;
+  }
+  
+  // Setters
+  public void setName(String name) {
+	  this.name = name;
+  }
+  
+  // Prompts a user to input car details and adds to inventory list
+  public void addCar(Scanner scnr) {
+	  Automobile car = new Automobile();
+	  car.inputCarDetails(scnr);
+	  this.inventory.add(car);
+	  System.out.println(car.getMake() + " " + car.getModel() + " has been added to the inventory.");
+  }
+  
+  // Will iterate over 
+  public void removeCar(Scanner scnr) {
+	  // Print message if no array list size is 0
+	  if(this.inventory.size() == 0) {
+		  System.out.println("There are no cars in the inventory to remove.");
+		  return;
+	  }
+	  Validator validator = new Validator();
+	  String vin;
+	  
+	  // Get user input and validate input, loop if incorrect
+	  while (true) {
+		  System.out.println("Enter the vin of the car you wish to remove: ");
+		  vin = scnr.nextLine().trim();
+		  validator.validateString(vin, "Please enter a valid vin");
+		  break;
+	  }
+	  
+	  // Loop through the array and remove car if provided vin is found.
+	  for(int i = 0; i < this.inventory.size(); i++) {
+		  if(this.inventory.get(i).getVin().equals(vin)) {
+			  Automobile removedCar = this.inventory.get(i);
+			  System.out.println("car removed from inventory; ");
+			  removedCar.displayCarDetails();
+			  return;
+		  }
+	  }
+	  // Let user know the vin was not found.
+	  System.out.println("No car found with that vin number please try again");
+  }
+  
+}
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,14 +1,72 @@
 package main;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.PrintWriter;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import dealership.Dealership;
 import automobile.Automobile;
 
 public class Main {
 	public static void main(String[] args) {
-		Automobile car = new Automobile();
-		
-		car.inputCarDetails();
-		
-		car.displayCarDetails();
-
+		Dealership dealership = new Dealership("Great dealership");
+		getUserInputs(dealership);
+	}
+	
+	public static void writeToFile(Dealership dealership) {	
+		try {
+			String fileName = "Dealer_Inventory.txt";
+			FileOutputStream fileStream = new FileOutputStream(fileName);
+			PrintWriter outFS = new PrintWriter(fileStream);
+			ArrayList<Automobile> inventory = dealership.getInventory();
+			
+			// loop through the inventory
+			for(int i = 0; i < inventory.size(); i++) {
+				outFS.println("Car " + 1);
+				String[] carDetails = inventory.get(i).formatCarDetails();
+				
+				// Print car details to file
+				for(int j = 0; j< carDetails.length; j++) {
+					outFS.println(" - " + carDetails[j]);
+				}
+			}
+			
+			System.out.println("Inventory saved to " + fileName);
+			outFS.close();
+		} catch (FileNotFoundException e) {
+			System.out.println("An issue occured while writing to file.");
+		}
+	}
+	
+	public static  void getUserInputs(Dealership dealership) {
+		try (Scanner scnr = new Scanner(System.in)) {
+		while (true) {
+				System.out.println("What woud you like to do? Select a number.");
+				System.out.println("1) Add car to inventory");
+				System.out.println("2) Remove car from inventory");
+				System.out.println("3) Save inventory to file");
+				System.out.println("4) Exit the program");
+			
+				int choice = scnr.nextInt();
+				scnr.nextLine();
+				
+				switch (choice) {
+					case 1:
+						dealership.addCar(scnr);
+						break;
+					case 2:
+						dealership.removeCar(scnr);
+						break;
+					case 3: 
+						writeToFile(dealership);
+						break;
+					case 4:
+						System.out.println("Exiting the program");
+						return;
+				}
+				
+			}
+		}
 	}
 
 }
